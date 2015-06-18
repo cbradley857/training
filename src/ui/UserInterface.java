f67e57789809bf7e349e9c2936b1a131ca336734
neilrls <n.rafferty@kainos.com>
2015-06-18 14:31:43 +0100

    Show options method c reated

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -1,9 +1,19 @@
 package ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
+	private Scanner in;
+	
 	public UserInterface() {
-		// TODO Auto-generated constructor stub
+		in = new Scanner(System.in);
 	}
-
+	
+	public String showOptions()
+	{
+		System.out.println("Pick Option:");
+		System.out.println("[0] List Employees, [1] List Employee with given ID");
+		return in.next();
+	}
 }
