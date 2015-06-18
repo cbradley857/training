6c3a8984efa15d08930650c796b6f9b68acba079
Ibon Aranburu <ibona@kainos.com>
2015-06-18 14:45:56 +0100

    updated Inserts and added payable, consultant and sales employee to Source

--- Inserts.sql
+++ Inserts.sql
@@ -2,6 +2,5 @@ Insert into employee values(1, 'Peter Parker', 25000.00);
 Insert into employee values(2, 'Bruce Banner', 24000.00);
 Insert into employee values(3, 'Clark Kent', 23000.00);
 Insert into employee values(4, 'Bruce Wayne', 22000.00);
-Insert into employee values
-(5, 'Raven Darkholme', 21000.00);
+Insert into employee values(5, 'Raven Darkholme', 21000.00);
 
--- Source.sql
+++ Source.sql
@@ -1,2 +1,9 @@
 create database staff;
-create table employee(int id primary key, name varchar(32), salary decimal(9,2));
+use staff;
+create table employee(id int primary key, name varchar(32), salary decimal(9,2));
+create table payable(id int primary key, monthly_salary decimal (8,2));
+create table consultant(id int primary key, name varchar(32), daily_rate decimal (8,2), days_worked int);
+create table sales_employee(id int primary key, comission_rate decimal (2,2), sales_total decimal (9,2));
+
+
+
